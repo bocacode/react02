101577171556c133f4f06f23ac5147bf37ce8536
Todd Albert <todd@bocacode.com>
2021-05-03 09:54:06 -0400

    change Coffees to functional component with hooks

--- src/components/Coffees/index.js
+++ src/components/Coffees/index.js
@@ -1,39 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import SingleCoffee from './SingleCoffee'
 
-class Coffees extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      coffees: null
-    }
-  }
-  componentDidMount() {
-    fetch('https://api.sampleapis.com/coffee/iced')
-      .then(response => response.json())
-      .then(data => this.setState({ coffees: data }))
-      .catch(err => console.log(err))
-  }
-  handleButtonClick(type) {
-    this.setState({ coffees: null })
+function Coffees() {
+  const [coffees, setCoffees] = useState(null)
+  const [type, setType] = useState('hot')
+  useEffect(() => {
     fetch(`https://api.sampleapis.com/coffee/${type}`)
       .then(response => response.json())
-      .then(data => this.setState({ coffees: data }))
+      .then(data => setCoffees(data))
       .catch(err => console.log(err))
-  }
-  render() {
-    const { coffees } = this.state
-    return (
+  }, [type])
+  return (
       <>
         <h2>Coffees:</h2>
-        <button onClick={() => this.handleButtonClick('hot')}>HOT</button>
-        <button onClick={() => this.handleButtonClick('iced')}>ICED</button>
+        <button onClick={() => setType('hot')}>HOT</button>
+        <button onClick={() => setType('iced')}>ICED</button>
         {!coffees ? <p>Loading...</p> : coffees.map(coffee => {
           return <SingleCoffee key={coffee.id} coffee={coffee} />
         })}
       </>
-    )
-  }
+  )
 }
 
 export default Coffees
